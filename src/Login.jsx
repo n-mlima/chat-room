01821b3e96dbb00b69e7ed27d4e7acecf0582327
Nailson Lima <147450945+n-mlima@users.noreply.github.com>
2024-02-12 19:59:47 -0300

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BiHide, BiShow } from "react-icons/bi";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
